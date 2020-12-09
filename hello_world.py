389aee44c73b663c8065def3dd46c420838ecbe4
Pape khaly FAYE <papekhaly.faye@ynov.com>
2020-12-09 15:46:26 +0100

    v

--- hello_world.py
+++ hello_world.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'HELLO WORLD! FROM DEVOPS TEAM'
+    return 'HELLO WORLD! FROM DEVOPS TEAM !'
 
 if __name__ == '__main__':
     app.run()
